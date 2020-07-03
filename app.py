d06f781b0c95fa935eb1afb08bb51cd5453df00a
gsanthoshkrishna <gsanthoshkrishna@gmail.com>
2020-07-03 15:57:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def initial():
     return render_template("index.html",cur=cur)
 if __name__ == "__main__":
     app.secret_key="dwqwfewfwqdqw"
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="8222")

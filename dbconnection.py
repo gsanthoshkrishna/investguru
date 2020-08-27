89faf7e937d0f6c93d41fc5f30abe0da3934d388
Santhosh Gade <gsanthoshkrishna@gmail.com>
2020-08-27 22:22:35 +0530

    updated

--- dbconnection.py
+++ dbconnection.py
@@ -7,9 +7,9 @@ def executeQuery():
                         database="murthy")       
     cur = db.cursor()
     cur.execute("SELECT name,value,target FROM invest_data")
-    #csv_url = "https://www1.nseindia.com/content/historical/EQUITIES/2020/AUG/cm27AUG2020bhav.csv.zip"
-    #req = requests. get(csv_url)
-    #print req
+    csv_url = "https://www1.nseindia.com/content/historical/EQUITIES/2020/AUG/cm27AUG2020bhav.csv.zip"
+    req = requests. get(csv_url)
+    print req
     res=cur.fetchall()
     return res
 

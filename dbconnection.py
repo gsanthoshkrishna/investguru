96d988fc22773823b448c37034b6c9709a64ea72
santhosh Krishna <gsanthoshkrishna@gmail.com>
2020-09-01 12:17:48 +0530

    updated

--- dbconnection.py
+++ dbconnection.py
@@ -1,7 +1,7 @@
 import pymysql
 
 def executeQuery():
-    db = pymysql.connect(host="172.19.0.2",    
+    db = pymysql.connect(host="172.18.0.2",    
                         user="root", 
                         password="Pass@123",        
                         database="murthy")       
@@ -10,7 +10,7 @@ def executeQuery():
     res=cur.fetchall()
     return res
 def insertDailyData():
-    db = pymysql.connect(host="172.19.0.2",    
+    db = pymysql.connect(host="172.18.0.2",    
                         user="root", 
                         password="Pass@123",        
                         database="murthy")       
@@ -20,7 +20,7 @@ def insertDailyData():
     sql = "INSERT INTO history_data VALUES('SBICARD','EQ','824','846','823.55','838.9','837','819.7','2309206','1931891501.2','2020-08-28','78166')"
     cur.execute(sql)
     db.commit()
-    print(mycursor.rowcount, "record inserted.")
+    print(cur.rowcount, "record inserted.")
     
 #TODO Get the values from downloaded csv.
 def getCSVData():
